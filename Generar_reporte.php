8148f0a6d6bb71424e9922c0746657d98be23007
Salvador <svm89koky@gmail.com>
2019-08-27 18:15:02 -0500

    First commit

---
+++ Generar_reporte.php
@@ -0,0 +1,52 @@
+<?php
+require('fpdf/fpdf.php');
+
+
+class PDF extends FPDF
+{
+// Cabecera de página
+function Header()
+{
+    // Arial bold 15
+    $this->SetFont('Arial','B',18);
+    // Movernos a la derecha
+    $this->Cell(80);
+    // Título
+    $this->Cell(30,10,'Reporte de Ventas Diarias ',0,0,'C');
+    // Salto de línea
+    $this->Ln(20);
+}
+
+// Pie de página
+function Footer()
+{
+    // Posición: a 1,5 cm del final
+    $this->SetY(-15);
+    // Arial italic 8
+    $this->SetFont('Arial','I',8);
+    // Número de página
+    $this->Cell(0,10,utf8_decode('Pagina').$this->PageNo().'/{nb}',0,0,'C');
+}
+}
+
+require 'cn.php';
+$consulta = "SELECT * FROM sales";
+$resultado = $mysqli->query($consulta);
+
+$pdf = new PDF();
+$pdf -> AliasNbPages();
+$pdf->AddPage();
+$pdf->SetFont('Arial','B',16);
+
+while($row = $resultado->fetch_assoc()){
+    
+    $pdf->Cell(50, 10, $row['product_id'], 1, 0, 'C', 0);
+    $pdf->Cell(30, 10, $row['qty'], 1, 0, 'C', 0);
+    $pdf->Cell(30, 10, $row['price'], 1, 1, 'C', 0);
+    $pdf->Cell(30, 10, $row['date'], 1, 1, 'C', 0); 
+
+}
+
+$pdf->Output();
+
+?>
---
+++ layouts/admin_menu.php
@@ -0,0 +1,54 @@
+<ul>
+  <li>
+    <a href="admin.php">
+      <i class="glyphicon glyphicon-home"></i>
+      <span>Inicio</span>
+    </a>
+  </li>
+  <li>
+    <a href="#" class="submenu-toggle" class="colorPaginaPrincipal">
+      <i class="glyphicon glyphicon-user"></i>
+      <span>Accesos</span>
+    </a>
+    <ul class="nav submenu">
+      <li><a href="group.php" style="color:white;">Administrar grupos</a> </li>
+      <li><a href="users.php">Administrar usuarios</a> </li>
+   </ul>
+  </li>
+  <li>
+    <a href="categorie.php" >
+      <i class="glyphicon glyphicon-list-alt"></i>
+      <span>Categorías</span>
+    </a>
+  </li>
+  <li>
+    <a href="#" class="submenu-toggle">
+      <i class="glyphicon glyphicon-qrcode"></i>
+      <span>Productos</span>
+    </a>
+    <ul class="nav submenu">
+       <li><a href="product.php">Administrar productos</a> </li>
+       <li><a href="add_product.php">Agregar productos</a> </li>
+   </ul>
+  </li>
+
+  <li>
+    <a href="#" class="submenu-toggle">
+      <i class="glyphicon glyphicon-shopping-cart"></i>
+       <span>Ventas</span>
+      </a>
+      <ul class="nav submenu">
+         <li><a href="sales.php">Administrar ventas</a> </li>
+         <li><a href="add_sale.php">Agregar venta</a> </li>
+     </ul>
+  </li>
+  <li>
+    <a href="#" class="submenu-toggle">
+      <i class="glyphicon glyphicon-piggy-bank"></i>
+       <span>Reporte de ventas</span>
+      </a>
+      <ul class="nav submenu">
+        <li><a href="Generar_reporte.php">Reporte de Ventas diarias</a> </li>
+      </ul>
+  </li>
+</ul>
